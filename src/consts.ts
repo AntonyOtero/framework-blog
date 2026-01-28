fb8726a5251016caa4e0508580cec4128384ff43
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2026-01-28 02:12:12 -0600

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'A Framework Blog';
-export const SITE_DESCRIPTION = 'Live out your myth!';
+export const SITE_TITLE = 'Devotional Metaphysics';
+export const SITE_DESCRIPTION = 'Devoid of purpose, repurpose the void.';
